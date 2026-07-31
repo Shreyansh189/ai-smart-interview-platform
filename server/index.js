fef84a1af5e21fd571346349e22bec9a92931dc5
Shreyansh Ghanekar <shreyansh0611@gmail.com>
2026-07-31 17:16:00 +0530

    feat: add home page UI

--- server/index.js
+++ server/index.js
@@ -8,16 +8,16 @@ import userRouter from "./routes/user.routes.js";
 
 const app = express();
 app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true
+  origin: "http://localhost:5173",
+  credentials: true
 }))
 
 
 app.use(express.json())
 app.use(cookieParser())
 
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
+app.use("/api/auth", authRouter)
+app.use("/api/user", userRouter)
 
 
 const PORT = process.env.PORT || 6000;
